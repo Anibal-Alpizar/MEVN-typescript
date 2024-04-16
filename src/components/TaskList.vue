d71fa593208600e73c52260de256025c1ba61d5c
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-04-15 22:28:34 -0600

    feat: Add TaskList component for displaying tasks

---
+++ src/components/TaskList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="task in tasks" :key="task._id">
+      {{ task.title }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { getTasks } from "../services/TaskService";
+import { Task } from "../interfaces/Task";
+
+export default defineComponent({
+  data() {
+    return {
+      tasks: [] as Task[],
+    };
+  },
+  methods: {
+    async loadTasks() {
+      const res = await getTasks();
+      this.tasks = res.data;
+    },
+  },
+  mounted() {
+    this.loadTasks();
+  },
+});
+</script>
--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,5 +1,9 @@
 import { Task } from "../interfaces/Task";
 import axios from "./axios";
+import { AxiosResponse } from "axios";
 
 export const createTask = async (task: Task) =>
   await axios.post("/tasks", task);
+
+export const getTasks = async (): Promise<AxiosResponse<Task[]>> =>
+  await axios.get("/tasks");
